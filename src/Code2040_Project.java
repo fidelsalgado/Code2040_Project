2b3265f95236c448a7d2df7f72e5bad2f8e9c54e
Fidel Salgado <fidels@stanford.edu>
2014-11-25 19:48:23 -0800

    registration code working

--- src/Code2040_Project.java
+++ src/Code2040_Project.java
@@ -1,23 +1,44 @@
 import java.net.*;
 import java.io.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
 
 public class Code2040_Project {
 
 	public static void main(String[] args) throws Exception {
+		String token = register();
+		System.out.println(token);
+		
+	}
+
+	public static String register() throws Exception {
+		//Creating JSON Object
+		JSONObject dictionary = new JSONObject();
+		dictionary.put("email", "fidels@stanford.edu");
+		dictionary.put("github", "https://github.com/fidels7/Code2040_Project.git");
+
+		//HTTP Request
 		URL url = new URL("http://challenge.code2040.org/api/register");
 		URLConnection conn = url.openConnection();
 		conn.setDoOutput(true);
 		OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
 
-		writer.write("value=1&anotherValue=1");
+		writer.write(dictionary.toString());
 		writer.flush();
 		String line;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		while ((line = reader.readLine()) != null) {
-			System.out.println(line);
-		}
+
+		//Parsing
+		line = reader.readLine();
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(line);
+		JSONObject jsonObject = (JSONObject) obj;
+
+		//Closing streams
 		writer.close();
 		reader.close();
 
+		return (String) jsonObject.get("result");
 	}
 }
